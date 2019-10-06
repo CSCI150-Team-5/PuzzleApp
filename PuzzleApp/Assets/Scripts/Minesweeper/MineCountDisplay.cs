02f2e556f645dabfb439778cd9a1872c0ba5f9ca
roberthov <48002190+roberthov@users.noreply.github.com>
2019-10-06 15:01:14 -0700

    Current code of Minesweeper.

---
+++ PuzzleApp/Assets/Scripts/Minesweeper/MineCountDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MineCountDisplay : MonoBehaviour
+{
+	public Text minesLeftText;
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (MineBoard.firstClick == true) minesLeftText.text = "Mines Left:" + "?";
+		else if (MineBoard.gameLost == false) minesLeftText.text = "Mines Left:" + MineBoard.totalMinesLeft();
+	}
+}
