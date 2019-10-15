dcf2c841bf26c5bf22db745bf92ae205d470cf1e
Gerry300 <48077359+Gerry300@users.noreply.github.com>
2019-10-15 15:32:48 -0700

    AutoDrop Complete(?)
    It is functionla, but needs some work

---
+++ PuzzleApp/Assets/AutoDrop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDrop : MonoBehaviour
+{
+    private float previousTime;
+    public float fallTime = 0.8f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        downFall();
+    }
+
+    public void downFall()
+    {
+        if (Time.time - previousTime > fallTime)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1);
+            previousTime = Time.time;
+        }
+    }
+}
---
+++ PuzzleApp/Assets/PieceMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void MoveLeft()
+    {
+        transform.position = new Vector3(transform.position.x - 1, transform.position.y);
+    }
+ 
+    public void MoveRight()
+    {
+        transform.position = new Vector3(transform.position.x + 1, transform.position.y);
+    }
+}
