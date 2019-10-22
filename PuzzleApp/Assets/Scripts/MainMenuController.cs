28c86ccd97669749898426dae255eaa8dda1a140
Marc <normal42@mail.fresnostate.edu>
2019-10-22 16:09:25 -0700

    Redid Menu to be non-static
    The menu now must be called from each game.

--- PuzzleApp/Assets/Scripts/MainMenuController.cs
+++ PuzzleApp/Assets/Scripts/MainMenuController.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject myMasterMenu; //Make a place for the Master Menu Prefab - Place the prefab in here in the Unity UI
     private void Awake()
     {
         Screen.orientation = ScreenOrientation.Portrait;
+        Instantiate(myMasterMenu, new Vector3(0, 0, 0), Quaternion.identity); //Create the menu and place it in its default position
+        MasterMenuBehavior.OnClickedSettings += ClickedSettings; //Subscribe your function to be notified when setting button is clicked
+        MasterMenuBehavior.OnClickedBack += ClickedBack; //Subscribe your function to be notified when back button is clicked
     }
     public void OpenMinesweeper()
     {
@@ -16,5 +20,15 @@ public class MainMenuController : MonoBehaviour
     public void OpenSnake()
     {
         SceneManager.LoadScene(3);
+    }
+
+    void ClickedSettings() //Handle Settings Button Clicks Here
+    {
+        Debug.Log("Clicked Settings Button");
+    }
+
+    void ClickedBack() //Handle Back Button Clicks Here
+    {
+        Debug.Log("Clicked Back Button");
     }
 }
---
+++ PuzzleApp/Assets/Scripts/MasterMenuBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterMenuBehavior : MonoBehaviour
+{
+    public delegate void ClickedSettings();
+    public delegate void ClickedBack();
+
+    public static event ClickedSettings OnClickedSettings;
+    public static event ClickedBack OnClickedBack;
+
+    [SerializeField]
+    private Button buttonSettings;
+    [SerializeField]
+    private Button buttonBack;
+    [SerializeField]
+    private Text text;
+
+    void Awake()
+    {
+        buttonSettings.onClick.AddListener(RunSettings);
+        buttonBack.onClick.AddListener(RunBack);
+    }
+
+    void RunSettings()
+    {
+        if (OnClickedSettings != null) OnClickedSettings();
+    }
+
+    void RunBack()
+    {
+        if (OnClickedBack != null) OnClickedBack();
+    }
+}
