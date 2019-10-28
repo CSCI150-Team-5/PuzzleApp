9bc8a604e24cbae8f20af2b40138e72d1cadb891
Gerry300 <48077359+Gerry300@users.noreply.github.com>
2019-10-28 14:02:27 -0700

    Better Base

---
+++ PuzzleApp/PuzzleApp-Development/Project3/Project3/Test.cpp
@@ -0,0 +1,15 @@
+//Marc was here
+//or was he?
+#include <iostream>
+using namespace std;
+///here is a comment on this
+///Another comment
+
+int main() {
+	cout << "Hello World" << endl;
+	cout << "gummy code" << endl;
+	
+	system("pause");
+	return 0;
+
+}
