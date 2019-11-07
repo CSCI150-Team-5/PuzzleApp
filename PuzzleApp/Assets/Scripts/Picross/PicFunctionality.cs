192870ce3699940f2eefe29a5a9438c5a7feaaa2
roberthov <roberthov@users.noreply.github.com>
2019-11-06 16:44:15 -0800

    Picross Update, beginning work on possible AI

--- PuzzleApp/Assets/Scripts/Picross/PicFunctionality.cs
+++ PuzzleApp/Assets/Scripts/Picross/PicFunctionality.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class PicFunctionality : MonoBehaviour
 {
+<<<<<<< Updated upstream
 	public GameObject[] menuObjects;    //0: Select Difficulty Label	1: 5x5 Difficulty Button
+=======
+ 	public GameObject[] menuObjects;	//0: Select Difficulty Label	1: 5x5 Difficulty Button
+>>>>>>> Stashed changes
 	public GameObject[] gameUIObjects;  //0: 
 
 	public void deactivateMenu(bool menuState)
@@ -19,6 +23,7 @@ public class PicFunctionality : MonoBehaviour
 		PicAssets.instance.board = board = new PicBoard(PicAssets.instance.w, PicAssets.instance.h);
 
 		Screen.orientation = ScreenOrientation.Portrait;
+<<<<<<< Updated upstream
 		//		int greater;
 		//		if (w > h) greater = w;
 		//		else greater = h;
@@ -35,6 +40,24 @@ public class PicFunctionality : MonoBehaviour
 		////		can.transform.offsetMax = PicAssets.instance.can.transform.offsetMax;
 		////		can.transform.offsetMin = PicAssets.instance.can.transform.offsetMin;
 		//		can.transform.localScale = PicAssets.instance.can.transform.localScale;
+=======
+//		int greater;
+//		if (w > h) greater = w;
+//		else greater = h;
+		PicAssets.instance.cam.orthographicSize = PicAssets.instance.w + (float)3;// (w > h) ? w : h;// greater;// MineAssets.instance.h;
+		PicAssets.instance.cam.transform.position = new Vector3(((float)PicAssets.instance.w / 2.0f) - ((PicAssets.instance.w < 10) ? 1f : 2.25f), ((float)PicAssets.instance.h / 2.0f) - 0.5f, -10);
+//		PicAssets.instance.can.transform.position = new Vector3(((float)PicAssets.instance.w / 2.0f) - ((PicAssets.instance.w < 10) ? 1f : 2.25f), ((float)PicAssets.instance.h / 2.0f) - 0.5f, -10);
+//		Camera.current.transform.Translate(new Vector3(1.0f, 1.0f, 1.0f));
+
+//		MineTile[] tiles = new MineTile[MineAssets.instance.w * MineAssets.instance.h];
+
+
+//		GameObject can = new GameObject("can", typeof(Canvas));
+//		can.transform.localPosition = PicAssets.instance.can.transform.localPosition;
+////		can.transform.offsetMax = PicAssets.instance.can.transform.offsetMax;
+////		can.transform.offsetMin = PicAssets.instance.can.transform.offsetMin;
+//		can.transform.localScale = PicAssets.instance.can.transform.localScale;
+>>>>>>> Stashed changes
 
 
 		for (int i = 0; i < PicAssets.instance.w; i++)
@@ -42,6 +65,7 @@ public class PicFunctionality : MonoBehaviour
 			for (int j = 0; j < PicAssets.instance.h; j++)
 			{
 				GameObject tile = new GameObject("Tile (" + i + "," + j + ")", typeof(SpriteRenderer));
+<<<<<<< Updated upstream
 				tile.transform.parent = PicAssets.instance.can.transform;
 				tile.GetComponent<SpriteRenderer>().sprite = PicAssets.instance.unfilledTexture;
 				tile.transform.position = new Vector3(i, j);//Coordinates need to be fixed.
@@ -49,6 +73,16 @@ public class PicFunctionality : MonoBehaviour
 				tile.AddComponent<BoxCollider2D>();
 				tile.AddComponent<PicTile>();
 				//				tiles[i * MineAssets.instance.w + j] = tile.AddComponent<MineTile>();
+=======
+//				tile.transform.parent = PicAssets.instance.can.transform;
+				tile.GetComponent<SpriteRenderer>().sprite = PicAssets.instance.unfilledTexture;
+				tile.transform.position = new Vector3(i, j);//Coordinates need to be fixed.
+				tile.tag = "PicObj";
+				tile.AddComponent<BoxCollider2D>();
+				tile.AddComponent<PicTile>();
+//				tiles[i * MineAssets.instance.w + j] = tile.AddComponent<MineTile>();
+				PicAssets.instance.tileObjects[i][j] = tile;
+>>>>>>> Stashed changes
 			}
 		}
 		////		int a = MineBoard.totalFlags();
@@ -99,7 +133,11 @@ public class PicFunctionality : MonoBehaviour
 		for (int colNumber = 0; colNumber < PicAssets.instance.w; colNumber++)
 		{
 			int current = 0;
+<<<<<<< Updated upstream
 			for (int i = PicAssets.instance.h - 1; i >= 0; i--)
+=======
+			for (int i = PicAssets.instance.h -1; i >= 0; i--)
+>>>>>>> Stashed changes
 			{
 				if (PicAssets.instance.tiles[colNumber][i].empty == true) current++;
 				else if (current != 0)
@@ -126,6 +164,7 @@ public class PicFunctionality : MonoBehaviour
 				colNumber--;
 			}
 		}
+<<<<<<< Updated upstream
 		//		for (int i = 0; i < PicAssets.instance.h; i++)
 		//		{
 		//			for (int j = 0; j < PicAssets.instance.tiles[i].Count; j++) Debug.Log("Row " + i + ", node " + j + ":	" + PicAssets.instance.tiles[i][j].empty);
@@ -134,6 +173,11 @@ public class PicFunctionality : MonoBehaviour
 		//		}
 		Debug.Log(PicAssets.instance.gameStart);
 		if (PicAssets.instance.gameStart) return;
+=======
+
+///		Debug.Log(PicAssets.instance.gameStart);
+		if(PicAssets.instance.gameStart) return;
+>>>>>>> Stashed changes
 		for (int i = 0; i < PicAssets.instance.h; i++)
 		{
 			GameObject obj = new GameObject("Row " + i + " Label", typeof(CanvasRenderer));
@@ -147,7 +191,11 @@ public class PicFunctionality : MonoBehaviour
 			obj.GetComponent<Text>().text = str;// "" + (-2) + "," + i + "";
 			obj.GetComponent<Text>().font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
 			obj.GetComponent<Text>().fontSize = (int)(87.5f - PicAssets.instance.w * 2.5f);
+<<<<<<< Updated upstream
 			obj.GetComponent<Text>().color = Color.black;//gray;
+=======
+			obj.GetComponent<Text>().color = (str == "0") ? Color.gray : Color.black;//gray;
+>>>>>>> Stashed changes
 			obj.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;// horizontalOverflow;
 			obj.GetComponent<Text>().alignment = TextAnchor.MiddleRight;
 
@@ -156,10 +204,19 @@ public class PicFunctionality : MonoBehaviour
 			obj.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0.5f);
 			obj.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0.5f);
 
+<<<<<<< Updated upstream
 			obj.tag = "PicObject";
 
 			obj.transform.localScale = new Vector3(1, 1, 1);
 			obj.transform.position = new Vector3((PicAssets.instance.w < 10) ? -1f : -1.5f, i, -5);
+=======
+			obj.tag = "PicObj";
+
+			obj.transform.localScale = new Vector3(1,1,1);
+			obj.transform.position = new Vector3((PicAssets.instance.w < 10) ? -1f : -1.5f,i,-5);
+
+			PicAssets.instance.rowObjects.Add(obj);
+>>>>>>> Stashed changes
 		}
 
 		for (int i = 0; i < PicAssets.instance.w; i++)
@@ -175,7 +232,11 @@ public class PicFunctionality : MonoBehaviour
 			obj.GetComponent<Text>().text = str;
 			obj.GetComponent<Text>().font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
 			obj.GetComponent<Text>().fontSize = (int)(87.5f - PicAssets.instance.w * 2.5f);
+<<<<<<< Updated upstream
 			obj.GetComponent<Text>().color = Color.black;//gray;
+=======
+			obj.GetComponent<Text>().color = (str == "0") ? Color.gray : Color.black;//gray;
+>>>>>>> Stashed changes
 			obj.GetComponent<Text>().verticalOverflow = VerticalWrapMode.Overflow;
 			obj.GetComponent<Text>().alignment = TextAnchor.LowerCenter;
 
@@ -184,11 +245,22 @@ public class PicFunctionality : MonoBehaviour
 			obj.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0.5f);
 			obj.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0.5f);
 
+<<<<<<< Updated upstream
 			obj.tag = "PicObject";
 
 			obj.transform.localScale = new Vector3(1, 1, 1);
 			obj.transform.position = new Vector3(i, ((PicAssets.instance.w < 10) ? 0f : 0.5f) + (float)PicAssets.instance.h, -5);
 		}
+=======
+			obj.tag = "PicObj";
+
+			obj.transform.localScale = new Vector3(1,1,1);
+			obj.transform.position = new Vector3(i,((PicAssets.instance.w < 10) ? 0f : 0.5f) + (float)PicAssets.instance.h, -5);
+
+			PicAssets.instance.columnObjects.Add(obj);
+		}
+		PicAI.operateOnRow(0);
+>>>>>>> Stashed changes
 	}
 
 	//	public void waitForTiles()
@@ -246,6 +318,7 @@ public class PicFunctionality : MonoBehaviour
 
 	public void newGame()
 	{
+<<<<<<< Updated upstream
 		Debug.Log("Hello! ! ?");
 		foreach (GameObject tile in GameObject.FindGameObjectsWithTag("PicObject"))
 			Destroy(tile);
@@ -257,6 +330,23 @@ public class PicFunctionality : MonoBehaviour
 		//		PicAssets.instance.minesLeftText.text = "?";
 		//		PicAssets.instance.timerText.text = "?";
 		//		PicAssets.instance.timerInt = 0;
+=======
+		foreach (GameObject tile in GameObject.FindGameObjectsWithTag("PicObj"))
+			Destroy(tile);
+
+//		foreach (Vector3 vec in PicAssets.instance.history)
+//			Debug.Log("History: " + (vec.x == 1f ? "Marked" : "Flagged" ) + "	(" + vec.y + "," + vec.z + ")");
+		PicAssets.instance.history.Clear();
+		PicAssets.instance.rehistory.Clear();
+
+//		PicAssets.instance.firstClick = true;
+//		PicAssets.instance.gameWon = false;
+//		PicAssets.instance.gameLost = false;
+
+//		PicAssets.instance.minesLeftText.text = "?";
+//		PicAssets.instance.timerText.text = "?";
+//		PicAssets.instance.timerInt = 0;
+>>>>>>> Stashed changes
 
 		PicAssets.instance.gameStart = false;
 
@@ -265,6 +355,7 @@ public class PicFunctionality : MonoBehaviour
 
 	public void returnToMainMenu()
 	{
+<<<<<<< Updated upstream
 		foreach (GameObject tile in GameObject.FindGameObjectsWithTag("PicObject"))
 			Destroy(tile);
 
@@ -275,10 +366,56 @@ public class PicFunctionality : MonoBehaviour
 		//		MineAssets.instance.minesLeftText.text = "?";
 		//		MineAssets.instance.timerText.text = "?";
 		//		MineAssets.instance.timerInt = 0;
+=======
+		foreach (GameObject tile in GameObject.FindGameObjectsWithTag("PicObj"))
+			Destroy(tile);
+
+		PicAssets.instance.history.Clear();
+		PicAssets.instance.rehistory.Clear();
+
+//		MineAssets.instance.firstClick = true;
+//		MineAssets.instance.gameWon = false;
+//		MineAssets.instance.gameLost = false;
+
+//		MineAssets.instance.minesLeftText.text = "?";
+//		MineAssets.instance.timerText.text = "?";
+//		MineAssets.instance.timerInt = 0;
+>>>>>>> Stashed changes
 
 		PicAssets.instance.board = null;
 
 		deactivateMenu(false);
 	}
 
+<<<<<<< Updated upstream
+=======
+	public void undo()
+	{
+		if (PicAssets.instance.history.Count == 0) return;
+		Vector3 toUndo = PicAssets.instance.history[PicAssets.instance.history.Count - 1];
+		PicAssets.instance.history.RemoveAt(PicAssets.instance.history.Count - 1);
+		PicAssets.instance.rehistory.Add(toUndo);
+
+		if (toUndo.x == 1f)			PicAssets.instance.tiles[(int)toUndo.y][(int)toUndo.z].fill();
+//		else if (toUndo.x == 0f)	PicAssets.instance.tiles[(int)toUndo.y][(int)toUndo.z].flag();
+		else						PicAssets.instance.tiles[(int)toUndo.y][(int)toUndo.z].flag();
+	}
+
+	public void redo()
+	{
+		if (PicAssets.instance.rehistory.Count == 0) return;
+		Vector3 toRedo = PicAssets.instance.rehistory[PicAssets.instance.rehistory.Count - 1];
+		PicAssets.instance.rehistory.RemoveAt(PicAssets.instance.rehistory.Count - 1);
+		PicAssets.instance.history.Add(toRedo);
+
+		if (toRedo.x == 1f)			PicAssets.instance.tiles[(int)toRedo.y][(int)toRedo.z].fill();
+//		else if (toRedo.x == 0f)	PicAssets.instance.tiles[(int)toRedo.y][(int)toRedo.z].flag();
+		else						PicAssets.instance.tiles[(int)toRedo.y][(int)toRedo.z].flag();
+	}
+
+	void Update()
+	{
+		Debug.Log("TESTING "+PicAI.longestEmptyStreak(false,0,0,PicAssets.instance.w));
+	}
+>>>>>>> Stashed changes
 }
--- PuzzleApp/Assets/Scripts/Picross/PicTile.cs
+++ PuzzleApp/Assets/Scripts/Picross/PicTile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 
 public class PicTile : MonoBehaviour
 {
@@ -10,6 +11,18 @@ public class PicTile : MonoBehaviour
 
 	public int xpos = -1;   //The coordinates of the tile.
 	public int ypos = -1;   //We get them from the tile's coordinates in the world.
+=======
+using UnityEngine.UI;
+
+public class PicTile : MonoBehaviour
+{
+	public bool empty = false;		//This bool indicates whether this tile is empty or not.
+	public bool filled = false;		//This bool can be turned on/off by the player to fill the tile.
+	public bool flagged = false;	//This bool can be turned on/off by the player to flag the tile.
+
+	public int xpos = -1;	//The coordinates of the tile.
+	public int ypos = -1;	//We get them from the tile's coordinates in the world.
+>>>>>>> Stashed changes
 
 	public PicBoard board = null;
 
@@ -19,20 +32,35 @@ public class PicTile : MonoBehaviour
 
 	public void fill()
 	{
+<<<<<<< Updated upstream
 		GetComponent<SpriteRenderer>().sprite = filled ? PicAssets.instance.unfilledTexture : PicAssets.instance.filledTexture;
 		filled = !filled;
 		flagged = !filled;
+=======
+		GetComponent<SpriteRenderer>().sprite = filled ? (flagged ? PicAssets.instance.flaggedTexture : PicAssets.instance.unfilledTexture) : PicAssets.instance.filledTexture;
+		filled = !filled;
+		PicAssets.instance.rowObjects[ypos].GetComponent<Text>().color = PicAssets.instance.board.checkRow(ypos) == true ? Color.gray : Color.black;
+		PicAssets.instance.columnObjects[xpos].GetComponent<Text>().color = PicAssets.instance.board.checkColumn(xpos) == true ? Color.gray : Color.black;
+>>>>>>> Stashed changes
 	}
 
 	public void flag()
 	{
+<<<<<<< Updated upstream
 		GetComponent<SpriteRenderer>().sprite = flagged ? PicAssets.instance.unfilledTexture : PicAssets.instance.flaggedTexture;
 		flagged = !flagged;
 		filled = !flagged;
+=======
+		GetComponent<SpriteRenderer>().sprite = flagged ? (filled ? PicAssets.instance.filledTexture : PicAssets.instance.unfilledTexture) : PicAssets.instance.flaggedTexture;
+		flagged = !flagged;
+		PicAssets.instance.rowObjects[ypos].GetComponent<Text>().color = PicAssets.instance.board.checkRow(ypos) == true ? Color.gray : Color.black;
+		PicAssets.instance.columnObjects[xpos].GetComponent<Text>().color = PicAssets.instance.board.checkColumn(xpos) == true ? Color.gray : Color.black;
+>>>>>>> Stashed changes
 	}
 
 	//	void Start()
 	void Awake()
+<<<<<<< Updated upstream
 	//	public PicTile()
 	{
 		empty = Random.value < PicAssets.instance.emptyFrequency;   //Each tile is initialized with a 15% chance of being empty.
@@ -50,16 +78,44 @@ public class PicTile : MonoBehaviour
 		board = PicAssets.instance.board;
 		//		if(empty) { GetComponent<SpriteRenderer>().sprite = PicAssets.instance.filledTexture; filled = true; }///
 	}
+=======
+//	public PicTile()
+	{
+		empty = Random.value < PicAssets.instance.emptyFrequency;	//Each tile is initialized with a 15% chance of being empty.
+
+		xpos = (int)transform.position.x;							//Storing the game world coordinates of the tile in the class variables.
+		ypos = (int)transform.position.y;							//These will be used to identify the tile.
+
+		w = PicAssets.instance.w;
+		h = PicAssets.instance.h;
+//		PicAssets.instance.tiles[xpos, ypos] = this;                //Making the game board reference this object, using the coordinates to identify it.
+//		PicAssets.instance.tiles[xpos].Add(this);                //Making the game board reference this object, using the coordinates to identify it.
+		PicAssets.instance.tiles[xpos][ypos] = this;                //Making the game board reference this object, using the coordinates to identify it.
+//		Debug.Log("" + (PicAssets.instance.tiles[xpos][ypos].empty == empty) + "	" + empty);
+
+		board = PicAssets.instance.board;
+//		if(empty) { GetComponent<SpriteRenderer>().sprite = PicAssets.instance.filledTexture; filled = true; }///
+		}
+>>>>>>> Stashed changes
 
 	void OnMouseOver()
 	{
 		if (PicAssets.instance.gameStart) return;
 		if (PicAssets.instance.gameWon) return; //If the game is over, return because we shouldn't allow any tiles to be flagged or filled.
 
+<<<<<<< Updated upstream
 		if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 ? Input.GetTouch(0).deltaTime >= 0.8f : false))    //If we right clicked.
 		{
 			flag();                         //Flag the selected tile.
 											///			PicBoard.checkCompletion();		//Then check if we flagged the last empty spot, and if we did, tell the user that they won.
+=======
+		if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 ? Input.GetTouch(0).deltaTime >= 0.8f : false))	//If we right clicked.
+		{
+			flag();                         //Flag the selected tile.
+///			PicBoard.checkCompletion();		//Then check if we flagged the last empty spot, and if we did, tell the user that they won.
+			PicAssets.instance.history.Add(new Vector3(0f,xpos,ypos));
+			PicAssets.instance.rehistory.Clear();
+>>>>>>> Stashed changes
 			return;
 		}
 
@@ -67,6 +123,13 @@ public class PicTile : MonoBehaviour
 		{
 			//MineAssets.instance.board.check(xpos, ypos);
 			fill();
+<<<<<<< Updated upstream
+=======
+			PicAssets.instance.history.Add(new Vector3(1f, xpos, ypos));
+			PicAssets.instance.rehistory.Clear();
+//			Debug.Log(PicAssets.instance.board.checkRow(ypos));
+//			PicAssets.instance.board.checkRow(ypos) == true ? PicAssets.instance.rows[i]
+>>>>>>> Stashed changes
 		}
 
 	}
