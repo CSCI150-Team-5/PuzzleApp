6df107d5737c874cec34adc9347f4f3789e660bf
Gerry300 <48077359+Gerry300@users.noreply.github.com>
2019-11-17 16:17:34 -0800

    Not much was changed to the game

--- PuzzleApp/Assets/Prefabs/Tetris/Tetris_Block.cs
+++ PuzzleApp/Assets/Prefabs/Tetris/Tetris_Block.cs
@@ -7,20 +7,20 @@ public class Tetris_Block : MonoBehaviour
     public Vector3 rotationPoint;
     private float previousTime;
     public float fallTime = 0.1f;
-    public static int height = 22;
-    public static int width = 10;
+    public static int height = 24;
+    public static int width = 9;
     private static Transform[,] grid = new Transform [width, height];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-0.5f, 0, 0);
             if (!isValid())
@@ -28,8 +28,8 @@ public class Tetris_Block : MonoBehaviour
                 transform.position += new Vector3(0.5f, 0, 0);
             }
         }
-        
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(0.5f, 0, 0);
             if (!isValid())
@@ -37,26 +37,27 @@ public class Tetris_Block : MonoBehaviour
                 transform.position += new Vector3(-0.5f, 0, 0);
             }
         }
-        
+
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 0.5f), 90);
             if (!isValid())
             {
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 0.5f), -90);
             }
         }
 
         if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
         {
-            transform.position += new Vector3(0, -0.5f, 0);
+            transform.position += new Vector3(0, -1, 0);
             if (!isValid())
             {
-                transform.position += new Vector3(0, 0.5f, 0);
-                Add2Grid();
+                transform.position += new Vector3(0, 1, 0);
                 this.enabled = false;
+                Add2Grid();
                 FindObjectOfType<Spawn_Tetromino>().NewTetromino();
             }
+
             previousTime = Time.time;
         }
     }
@@ -76,15 +77,15 @@ public class Tetris_Block : MonoBehaviour
     {
         foreach (Transform children in transform)
         {
-            int roundedX = (int)(children.transform.position.x);
-            int roundedY = (int)(children.transform.position.y);
+            int roundedX = (int) (children.transform.position.x);
+            int roundedY = (int) (children.transform.position.y);
 
-            if(roundedX < 1 || roundedX >= width || roundedY < 1 || roundedY >= height)
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
             {
                 return false;
             }
 
-            if(grid[roundedX, roundedY] != null)
+            if (grid[roundedX, roundedY] != null)
             {
                 return false;
             }
