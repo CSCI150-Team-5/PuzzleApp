cf93927a07facc579576b2f739c6a626016ad019
acmoua <54963439+acmoua@users.noreply.github.com>
2019-11-18 23:28:37 -0800

    lost old assest in branch switch....restarting....

---
+++ PuzzleApp/Assets/flipperController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipperController : MonoBehaviour
+{
+
+    public bool flipper;
+    // Update is called once per frame
+    void Update()
+    {
+        if ((flipper && Input.GetKey(KeyCode.RightControl)) || (!flipper && Input.GetKey(KeyCode.LeftControl)))
+        {
+            GetComponent<HingeJoint2D>().useMotor = true;
+        }
+        else
+        {
+            GetComponent<HingeJoint2D>().useMotor = false;
+        }
+    }
+}
