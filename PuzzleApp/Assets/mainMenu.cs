e518c2c801171e75267052a9cda8765f8c5e7c5b
acmoua <54963439+acmoua@users.noreply.github.com>
2019-11-19 13:42:16 -0800

    added main menu and transition buttons

---
+++ PuzzleApp/Assets/mainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playFlowFree(){
+        SceneManager.LoadScene("Flowfree");
+    }
+
+     public void returnToFreeFlowMain(){
+        SceneManager.LoadScene("MainMenuFlowfree");
+    }
+}
